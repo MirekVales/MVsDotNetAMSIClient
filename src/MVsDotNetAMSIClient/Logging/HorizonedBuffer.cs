39dde803cda6571de25c11359e888eedaf11bc68
Mirek Vales <mirekvales@users.noreply.github.com>
2020-05-15 00:00:31 +0200

    Logging support

---
+++ src/MVsDotNetAMSIClient/Logging/HorizonedBuffer.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MVsDotNetAMSIClient.Logging
+{
+    internal class HorizonedBuffer : IEnumerable<int>
+    {
+        readonly int[] buffer;
+        readonly int capacity;
+
+        int writeCursor;
+
+        internal HorizonedBuffer(int capacity, int initialSize)
+        {
+            buffer = new int[capacity];
+            this.capacity = capacity;
+            buffer[0] = initialSize;
+        }
+
+        internal void Increase()
+            => buffer[writeCursor % capacity]++;
+        
+        internal void Move()
+        {
+            unchecked
+            {
+                buffer[++writeCursor % capacity] = 0;
+            }
+        }
+        internal void Move(int steps)
+        {
+            for (var i = 0; i < steps; i++)
+                Move();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+            => Enumerable
+               .Range(0, capacity)
+                .Select(i => buffer[(writeCursor + capacity - i) % capacity])
+                .GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
---
+++ src/MVsDotNetAMSIClient/Logging/HorizonedHistogram.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace MVsDotNetAMSIClient.Logging
+{
+    public class HorizonedHistogram<T> : IDisposable
+    {
+        public int Capacity { get; }
+        public TimeSpan Length { get; }
+        public TimeSpan Granularity { get; }
+
+        readonly Dictionary<T, HorizonedBuffer> store;
+        readonly ReaderWriterLockSlim readerWriterLockSlim;
+
+        DateTime lastUpdate;
+
+        public HorizonedHistogram(TimeSpan length, TimeSpan granularity)
+        {
+            store = new Dictionary<T, HorizonedBuffer>();
+            readerWriterLockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+            Length = length;
+            Granularity = granularity;
+
+            Capacity = (int)Math.Ceiling(length.Ticks / (double)granularity.Ticks);
+
+            lastUpdate = DateTime.Now;
+        }
+
+        public void Add(T instance)
+        {
+            try
+            {
+                readerWriterLockSlim.EnterUpgradeableReadLock();
+
+                if (IsMoveNecessary())
+                    Move();
+
+                if (store.TryGetValue(instance, out var value))
+                    value.Increase();
+                else
+                    InitializeNew(instance);
+            }
+            finally
+            {
+                readerWriterLockSlim.ExitUpgradeableReadLock();
+            }
+        }
+
+        void InitializeNew(T instance)
+        {
+            try
+            {
+                readerWriterLockSlim.EnterWriteLock();
+
+                store[instance] = new HorizonedBuffer(Capacity, 1);
+            }
+            finally
+            {
+                readerWriterLockSlim.ExitWriteLock();
+            }
+        }
+
+        bool IsMoveNecessary()
+            => (DateTime.Now - lastUpdate).Ticks > Granularity.Ticks;
+
+        void Move()
+        {
+            try
+            {
+                readerWriterLockSlim.EnterWriteLock();
+
+                var steps = (int)Math.Floor((DateTime.Now - lastUpdate).Ticks / (double)Granularity.Ticks);
+                foreach (var value in store.Values)
+                    value.Move(steps);
+
+                lastUpdate = DateTime.Now;
+            }
+            finally
+            {
+                readerWriterLockSlim.ExitWriteLock();
+            }
+        }
+
+        public IEnumerable<int> Get(T instance, TimeSpan offset, TimeSpan length)
+        {
+            try
+            {
+                readerWriterLockSlim.EnterReadLock();
+
+                if (store.TryGetValue(instance, out var buffer))
+                    return buffer
+                        .Skip((int)Math.Ceiling(offset.Ticks / (double)Granularity.Ticks))
+                        .Take((int)Math.Ceiling(length.Ticks / (double)Granularity.Ticks))
+                        .ToArray();
+
+                return Enumerable.Empty<int>();
+            }
+            finally
+            {
+                readerWriterLockSlim.ExitReadLock();
+            }
+        }
+
+        public void Dispose()
+            => readerWriterLockSlim?.Dispose();
+    }
+}
